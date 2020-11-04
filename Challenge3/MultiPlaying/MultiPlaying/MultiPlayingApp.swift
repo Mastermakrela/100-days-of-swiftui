8c2effed74b5ade31aa382a418b5aab72e1e932a
Mastermakrela <mastermakrela@gmail.com>
2020-11-04 22:45:07 +0100

    updated bundle identifiers

---
+++ Challenge3/MultiPlaying/MultiPlaying/MultiPlayingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiPlayingApp.swift
+//  MultiPlaying
+//
+//  Created by Krzysztof Kostrzewa on 04/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct MultiPlayingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
