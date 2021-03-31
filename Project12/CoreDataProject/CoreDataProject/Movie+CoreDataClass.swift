8513fe63f5c5ab4fd1fc014bb04a5617bfb48a9a
mastermakrela <mastermakrela@gmail.com>
2021-03-31 15:50:25 +0200

    Project 12 done(?)

---
+++ Project12/CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Krzysztof Kostrzewa on 23/11/2020.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
